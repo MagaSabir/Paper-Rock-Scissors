2f89f450a3c885eb91f76f837f7997d7075764ea
MSabir <magsabir95@gmail.com>
2024-04-21 14:51:27 +0300

    Create UI

--- main.js
+++ main.js
@@ -1,37 +1,63 @@
-function getComputerChoice() {
-	const value = ['rock', 'scissors', 'paper']
-	let randomNumber = Math.floor(Math.random() * 3)
-	return value[randomNumber]
-}
+const icons = document.querySelector('#icons');
+const ico = document.querySelector('.ico');
+const rock = document.querySelector('.rock');
+const computer = document.querySelector('.computer');
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('.scissors');
+const result = document.querySelector('.result')
 
-console.log(Math.floor(Math.random() * 3))
+
+function getComputerChoice() {
+	const value = ['rock', 'scissors', 'paper'];
+	let randomNumber = Math.floor(Math.random() * 3);
+	return value[randomNumber];
+}
 
 function playRound(playerSelection, computerSelection) {
-	if (playerSelection === 'paper' && computerSelection === 'rock') {
-		console.log(`You Win! ${playerSelection} beats ${computerSelection}`)
-	} else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-		console.log(`You Win! ${playerSelection} beats ${computerSelection}`)
+	if (playerSelection === 'rock' && computerSelection === 'scissors') {
+			result.textContent = `You Win! ${playerSelection} beats ${computerSelection}😀`;
+			result.style.color = 'gree'
 	} else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-		console.log(`You Win! ${playerSelection} beats ${computerSelection}`)
+		result.textContent = `You Win! ${playerSelection} beats ${computerSelection}😀`;
+		result.style.color = 'green'
+	} else if (playerSelection === 'paper' && computerSelection === 'rock') {
+		result.textContent = `You Win! ${playerSelection} beats ${computerSelection}😀`;
+		result.style.color = 'green'
 	} else if (playerSelection === computerSelection) {
-		console.log(`Draw! ${playerSelection} = ${computerSelection}`)
+		result.textContent = `Draw! ${playerSelection} = ${computerSelection}😑`;
+		result.style.color = 'blue'
 	} else {
-		console.log(`You Lose! ${computerSelection} beats ${playerSelection}`)
+		result.textContent = `You Lose! ${computerSelection} beats ${playerSelection}😔`;
+		result.style.color = 'red'
 	}
 }
+
+
+icons.addEventListener('click', function(e) {
+	ico.style.backgroundRepeat = 'no-repeat';
+	ico.style.backgroundColor = '';
+	if (e.target === paper || e.target.parentElement === paper) {
+			ico.style.backgroundImage = 'url(icons/paper.png)';
+			playerSelection = 'paper';
+	}
+	if (e.target === rock || e.target.parentElement === rock) {
+			ico.style.backgroundImage = 'url(icons/rock.png)';
+			playerSelection = 'rock';
+	}
+	if (e.target === scissors || e.target.parentElement === scissors) {
+			ico.style.backgroundImage = 'url(icons/scissors.png)';
+			playerSelection = 'scissors';
+	}
+	playGame();
+});
+
+
+
 
 function playGame() {
-	let playerSelection = prompt('Your choice').toLowerCase()
-	while (
-		playerSelection !== 'paper' &&
-		playerSelection !== 'rock' &&
-		playerSelection !== 'scissors'
-	) {
-		playerSelection = prompt('Incorrect input. Try again').toLowerCase()
-	}
-	let computerSelection = getComputerChoice()
-
-	playRound(playerSelection, computerSelection)
+	let computerSelection = getComputerChoice();
+	playRound(playerSelection, computerSelection);
+	computer.style.backgroundImage = `url(icons/${computerSelection}.png)`;
 }
 
-playGame()
+console.log(result)
